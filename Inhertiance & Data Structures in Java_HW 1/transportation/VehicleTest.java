be01e1fa62aff53cc50cf22b3d23f828d9a3d096
Yuming Gu <yug52@pitt.edu>
2022-07-14 00:48:00 -0400

    Update VehicleTest.java

--- Inhertiance & Data Structures in Java_HW 1/transportation/VehicleTest.java
+++ Inhertiance & Data Structures in Java_HW 1/transportation/VehicleTest.java
@@ -12,130 +12,147 @@ class VehicleTest {
 	Vehicle bike1;
 	Vehicle bike2;
 	Vehicle bike3;
-	
+
 	@BeforeEach
 	void setUp() throws Exception {
-		this.jeep1 = new Car("Jeep", 2019);
-		this.jeep2 = new Car("Jeep", 2010);
-		this.benz = new Car("Benz", 2020);
-		this.bike1 = new Bike("Trek", 1999);
-		this.bike2 = new Bike("Giant", 1999);
-		this.bike3 = new Bike("Giant", 2020);
+		this.jeep1 = new Car("Jeep", 2019); //$NON-NLS-1$
+		this.jeep2 = new Car("Jeep", 2010); //$NON-NLS-1$
+		this.benz = new Car("Benz", 2020); //$NON-NLS-1$
+		this.bike1 = new Bike("Trek", 1999); //$NON-NLS-1$
+		this.bike2 = new Bike("Giant", 1999); //$NON-NLS-1$
+		this.bike3 = new Bike("Giant", 2020); //$NON-NLS-1$
 	}
 
 	@Test
 	void testVehicle() {
-		assertEquals("Jeep", jeep1.brand);
-		assertEquals("Jeep", jeep2.brand);
-		assertEquals("Benz", benz.brand);
-		assertEquals("Trek", bike1.brand);
-		assertEquals("Giant", bike2.brand);
-		assertEquals("Giant", bike3.brand);
-		
-		assertEquals("Car", jeep1.type);
-		assertEquals("Car", jeep2.type);
-		assertEquals("Car", benz.type);
-		assertEquals("Bike", bike1.type);
-		assertEquals("Bike", bike2.type);
-		assertEquals("Bike", bike3.type);
-		
+		assertEquals("Jeep", this.jeep1.brand); //$NON-NLS-1$
+		assertEquals("Jeep", this.jeep2.brand); //$NON-NLS-1$
+		assertEquals("Benz", this.benz.brand); //$NON-NLS-1$
+		assertEquals("Trek", this.bike1.brand); //$NON-NLS-1$
+		assertEquals("Giant", this.bike2.brand); //$NON-NLS-1$
+		assertEquals("Giant", this.bike3.brand); //$NON-NLS-1$
+
+		assertEquals("Car", this.jeep1.type); //$NON-NLS-1$
+		assertEquals("Car", this.jeep2.type); //$NON-NLS-1$
+		assertEquals("Car", this.benz.type); //$NON-NLS-1$
+		assertEquals("Bike", this.bike1.type); //$NON-NLS-1$
+		assertEquals("Bike", this.bike2.type); //$NON-NLS-1$
+		assertEquals("Bike", this.bike3.type); //$NON-NLS-1$
+
 	}
 
 	@Test
 	void testGetAge() {
-		assertEquals(1, jeep1.getAge());
-		assertEquals(10, jeep2.getAge());
-		assertEquals(0, benz.getAge());
-		assertEquals(21, bike1.getAge());
-		assertEquals(21, bike2.getAge());
-		assertEquals(0, bike3.getAge());
+		assertEquals(1, this.jeep1.getAge());
+		assertEquals(10, this.jeep2.getAge());
+		assertEquals(0, this.benz.getAge());
+		assertEquals(21, this.bike1.getAge());
+		assertEquals(21, this.bike2.getAge());
+		assertEquals(0, this.bike3.getAge());
 	}
 
 	@Test
 	void testGetGasRemained() {
-		bike1.refuel(100);
-		assertEquals(0, bike1.getGasRemained());
-		bike1.refuel(10000);
-		assertEquals(0, bike1.getGasRemained());
-		jeep1.refuel(1);
-		assertEquals(1, jeep1.getGasRemained());
-		jeep1.refuel(100);
-		assertEquals(101, jeep1.getGasRemained());
-		jeep1.refuel(300);
-		assertEquals(200, jeep1.getGasRemained());
+		this.bike1.refuel(100);
+		assertEquals(0, this.bike1.getGasRemained());
+		this.bike1.refuel(10000);
+		assertEquals(0, this.bike1.getGasRemained());
+		this.jeep1.refuel(1);
+		assertEquals(1, this.jeep1.getGasRemained());
+		this.jeep1.refuel(100);
+		assertEquals(101, this.jeep1.getGasRemained());
+		this.jeep1.refuel(300);
+		assertEquals(200, this.jeep1.getGasRemained());
 	}
-	
+
 	@Test
 	void testGetTotalGasConsumed() {
-		bike2.run(100);
-		bike2.run(100);
-		bike2.run(100);
-		assertEquals(0, bike1.getTotalGasConsumed());
-		benz.run(10);
-		
-		assertEquals(0, benz.getTotalGasConsumed());
-		benz.refuel(100);
-		benz.run(5);
-		benz.run(5);
-		assertEquals(100, benz.getTotalGasConsumed());
-		benz.refuel(200);
-		benz.run(25);
-		assertEquals(300, benz.getTotalGasConsumed());
-		
-		jeep1.refuel(200);
-		jeep1.run(10);
-		assertEquals(100, jeep1.getTotalGasConsumed());
-		jeep1.run(15);
-		assertEquals(200, jeep1.getTotalGasConsumed());
-		jeep1.refuel(100);
-		assertEquals(200, jeep1.getTotalGasConsumed());
-		
+		this.bike2.run(100);
+		this.bike2.run(100);
+		this.bike2.run(100);
+		assertEquals(0, this.bike1.getTotalGasConsumed());
+		this.benz.run(10);
+
+		assertEquals(0, this.benz.getTotalGasConsumed());
+		this.benz.refuel(100);
+		this.benz.run(5);
+		this.benz.run(5);
+		assertEquals(100, this.benz.getTotalGasConsumed());
+		this.benz.refuel(200);
+		this.benz.run(25);
+		assertEquals(300, this.benz.getTotalGasConsumed());
+
+		this.jeep1.refuel(200);
+		this.jeep1.run(10);
+		assertEquals(100, this.jeep1.getTotalGasConsumed());
+		this.jeep1.run(15);
+		assertEquals(200, this.jeep1.getTotalGasConsumed());
+		this.jeep1.refuel(100);
+		assertEquals(200, this.jeep1.getTotalGasConsumed());
+
 	}
 
 	@Test
 	void testRefuel() {
-		bike1.refuel(100);
-		assertEquals(0, bike1.getGasRemained());
-		
-		jeep1.refuel(100);
-		assertEquals(100, jeep1.getGasRemained());
-		jeep1.refuel(101);
-		assertEquals(200, jeep1.getGasRemained());
-		
-		// TODO write at least 3 additional test cases 
-		
+		this.bike1.refuel(100);
+		assertEquals(0, this.bike1.getGasRemained());
+
+		this.jeep1.refuel(100);
+		assertEquals(100, this.jeep1.getGasRemained());
+		this.jeep1.refuel(101);
+		assertEquals(200, this.jeep1.getGasRemained());
+
+		this.bike1.refuel(0);
+		assertEquals(0, this.bike1.getGasRemained());
+
+		this.bike1.refuel(-50);
+		assertEquals(0, this.bike1.getGasRemained());
+
+		this.jeep1.refuel(0);
+		assertEquals(200, this.jeep1.getGasRemained());
+
 	}
-	
+
 	@Test
 	void testRun() {
-		bike2.run(100);
-		bike2.run(100);
-		bike2.run(100);
-		assertEquals(0, bike1.getGasRemained());
-		
-		benz.run(10);
-		assertEquals(0, benz.getGasRemained());
-		benz.refuel(100);
-		benz.run(5);
-		assertEquals(50, benz.getGasRemained());
-		
-		// TODO write at least 3 additional test cases 
+		this.bike2.run(100);
+		this.bike2.run(100);
+		this.bike2.run(100);
+		assertEquals(0, this.bike1.getGasRemained());
+
+		this.benz.run(10);
+		assertEquals(0, this.benz.getGasRemained());
+		this.benz.refuel(100);
+		this.benz.run(5);
+		assertEquals(50, this.benz.getGasRemained());
+
+		this.bike2.run(0);
+		assertEquals(0, this.bike1.getGasRemained());
+
+		this.bike2.run(-50);
+		assertEquals(0, this.bike1.getGasRemained());
+
+		this.benz.run(10);
+		assertEquals(0, this.benz.getGasRemained());
 	}
 
 	@Test
 	void testEquals() {
-		assertTrue(jeep1.equals(jeep2));
-		assertFalse(benz.equals(jeep2));
+		assertTrue(this.jeep1.equals(this.jeep2));
+		assertFalse(this.benz.equals(this.jeep2));
 
-		// TODO write at least 3 additional test cases 
+		assertFalse(this.bike2.equals(this.benz));
+		assertTrue(this.bike2.equals(this.bike2));
+		assertTrue(this.benz.equals(this.benz));
 	}
 
 	@Test
 	void testToString() {
-		assertEquals("Car Jeep", jeep1.toString());
-		assertEquals("Bike Trek", bike1.toString());
-		
-		// TODO write at least 3 additional test cases 
-		
+		assertEquals("Car Jeep", this.jeep1.toString()); //$NON-NLS-1$
+		assertEquals("Bike Trek", this.bike1.toString()); //$NON-NLS-1$
+
+		assertNotEquals("Bike Trek", this.jeep1.toString()); //$NON-NLS-1$
+		assertNotEquals("Car Jeep", this.bike1.toString()); //$NON-NLS-1$
+		assertNotEquals("Bike Trek", this.benz.toString()); //$NON-NLS-1$
 	}
 }
